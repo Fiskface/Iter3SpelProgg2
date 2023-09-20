6d5b337d3d713e4c0ee014e34a2afd9d8ad94251
Lukas <lukas.almgren03@gmail.com>
2023-09-20 19:43:42 +0200

    Enemy is Moving!
    Enemies can now move, and they also rotate to face the right way :)

--- Assets/Scripts/MoveEnemy.cs
+++ Assets/Scripts/MoveEnemy.cs
@@ -1,18 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class MoveEnemy : MonoBehaviour
 {
+    [SerializeField] private PathCheckpoints checkpointsSO;
+    
+    private int currentCheckpoint = -1;
+
+    [SerializeField] private float speed = 3f;
+
+    private Vector3 movingTowards;
+
+    private Vector3 direction;
     // Start is called before the first frame update
     void Start()
     {
-        
+        transform.position = checkpointsSO.GetNextCheckpoint(currentCheckpoint).position;
+        currentCheckpoint++;
+        SwapCheckpoint();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (MyMath.CloseOrPassed(transform.position, direction, movingTowards))
+        {
+            SwapCheckpoint();
+        }
+
+        transform.position += speed * Time.deltaTime * direction;
+    }
+
+    private void SwapCheckpoint()
+    {
+        var tempCheckpoint = checkpointsSO.GetNextCheckpoint(currentCheckpoint);
+        if(tempCheckpoint != null)
+        {
+            movingTowards = tempCheckpoint.position;
+            currentCheckpoint++;
+            direction = (movingTowards - transform.position).normalized;
+            transform.up = direction;
+        }
+        else
+        {
+            EnemyAtLastCheckpoint();
+        }
+    }
+
+    private void EnemyAtLastCheckpoint()
+    {
+        Debug.Log("Kill me");
     }
 }
---
+++ Assets/Scripts/MyMath.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MyMath
+{
+    public static bool CloseOrPassed(Vector3 position, Vector3 direction, Vector3 goal)
+    {
+        if ((position - goal).magnitude < 0.05)
+        {
+            return true;
+        }
+
+        if ((position - goal).normalized == direction.normalized)
+        {
+            return true;
+        }
+
+        return false;
+    }
+}
